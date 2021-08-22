6aef302cf073bf962f838336de907eb75f8398dc
John <lijiang01001@163.com>
2021-08-22 18:58:38 +0800

    add vertex buffer layout

--- QuarkEngine/QuarkEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ QuarkEngine/QuarkEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace Quark {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return mLayout; }
+		virtual void SetLayout(const BufferLayout& layout) override { mLayout = layout; }
 	private:
 		uint32_t mRendererID;
+		BufferLayout mLayout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
--- QuarkEngine/QuarkEngine/src/Quark/Application.cpp
+++ QuarkEngine/QuarkEngine/src/Quark/Application.cpp
@@ -13,6 +13,27 @@ namespace Quark {
 
 	Application* Application::sInstance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Quark::ShaderDataType::Float:    return GL_FLOAT;
+		case Quark::ShaderDataType::Float2:   return GL_FLOAT;
+		case Quark::ShaderDataType::Float3:   return GL_FLOAT;
+		case Quark::ShaderDataType::Float4:   return GL_FLOAT;
+		case Quark::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Quark::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Quark::ShaderDataType::Int:      return GL_INT;
+		case Quark::ShaderDataType::Int2:     return GL_INT;
+		case Quark::ShaderDataType::Int3:     return GL_INT;
+		case Quark::ShaderDataType::Int4:     return GL_INT;
+		case Quark::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		QK_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		QK_CORE_ASSERT(!sInstance, "Application already exists!");
@@ -27,16 +48,36 @@ namespace Quark {
 		glGenVertexArrays(1, &mVertexArray);
 		glBindVertexArray(mVertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		mVertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			mVertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = mVertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		mIndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -45,10 +86,13 @@ namespace Quark {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			out vec3 v_Position;
+			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -58,9 +102,11 @@ namespace Quark {
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
